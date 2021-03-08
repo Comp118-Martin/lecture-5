b035eee835ad774266abed0e9414fdcde85eddaf
Martin Ahoto <martinahoto4@gmail.com>
2021-03-08 09:32:23 +0200

    Initial Commit

---
+++ lecture 5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lecture 5
+//
+//  Created by MARTIN on 08/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
